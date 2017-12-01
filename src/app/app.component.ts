9e2c48f94f490ba287de9023117ecbbbb603b75d
Pierre Peinado <pierre.peinado@webinage.com>
2017-12-01 18:49:52 +0100

    app module component and template. Module includes all modules pages and providers of the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,32 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AuthHomePage } from './../pages/authentication/auth-home/auth-home';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+
+import { DataProvider } from './../providers/data';
+import { AuthenticationProvider } from './../providers/authentication';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
+  public isAppInitialized: boolean = false;
+  public user: IPersistedUser;
 
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    protected DataProvider: DataProvider,
+    protected AuthenticationProvider: AuthenticationProvider
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -31,14 +43,45 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.AuthenticationProvider.getCurrentUserData().subscribe(user => {
+        if (!this.isAppInitialized) {
+          this.nav.setRoot(HomePage);
+          this.isAppInitialized = true;
+        }
+        this.user = user;
+      }, error => {
+        this.nav.setRoot(AuthHomePage);
+      });
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
-  openPage(page) {
+  public openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  // public openGroupsPage(): void {
+  //   this.nav.setRoot(GroupListPage);
+  // }
+
+  // public openListsPage(): void {
+  //   this.Storage.get('currentUserId').then(currentUserId => this.nav.setRoot(ListListPage, [currentUserId, true]));
+  // }
+
+  // public openProfilePage(): void {
+  //   this.Storage.get('currentUserId').then(currentUserId => this.nav.setRoot(ProfilePage, [currentUserId, true]));
+  // }
+
+  // public openRelationshipsPage(): void {
+  //   this.Storage.get('currentUserId').then(currentUserId => this.nav.setRoot(RelationshipPage, [currentUserId, true]));
+  // }
+
+  public logout(): void {
+  	this.AuthenticationProvider.logout();
+    this.nav.setRoot(AuthHomePage);
   }
 }
